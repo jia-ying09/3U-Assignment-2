f88e449bde70cc267ef87ba6ea74dc0bec65fd63
linj4653 <linj4653@HRH-IC0027333.wrdsb.ca>
2017-02-17 10:15:13 -0500

    Assignment 2 first draft

--- src/A2Q3.java
+++ src/A2Q3.java
@@ -7,7 +7,6 @@ import becker.robots.Robot;
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author linj4653
@@ -23,27 +22,74 @@ public class A2Q3 {
         //creating robot
         Robot timmy = new Robot(kw, 4, 2, Direction.EAST);
         //moving to 0,0
-        while (true){ {
-        if(timmy.getStreet() == 0)
-         break;  }
-        while(true) {if (timmy.getDirection()== Direction.SOUTH)
-             timmy.turnLeft(); 
-             timmy.move();
-         if(timmy.getDirection()== Direction.WEST)
-             timmy.turnLeft();   
-         timmy.turnLeft();
-         timmy.move();      
-          while(true){ if(timmy.getAvenue()> 0)
-             timmy.move();             
-        while(true) if(timmy.getStreet()> 0)
-             timmy.move();  
-         if(timmy.getAvenue()== 0) 
-         timmy.turnLeft();
-            }
-          
-       
+        while (true) {
 
-        
-            
+
+            if (timmy.getDirection() == Direction.SOUTH) {
+                timmy.turnLeft();
+                timmy.turnLeft();
+                timmy.move();
+            }
+            timmy.turnLeft();
+            timmy.turnLeft();
+            timmy.move();
+            if (timmy.getDirection() == Direction.NORTH) {
+                timmy.turnLeft();
+                timmy.move();
+            }
+            if (timmy.getDirection() == Direction.WEST) {
+                timmy.turnLeft();
+                timmy.turnLeft();
+                timmy.move();
+                timmy.move();
+
+                while (true) {
+
+                    if (timmy.getAvenue() >= 0) {
+
+                        timmy.move();
+                    }
+
+                    if (timmy.getStreet() >= 0) {
+
+                        timmy.move();
+                        timmy.turnLeft();
+                    }
+
+                    if (timmy.getStreet() > 0) {
+
+                        timmy.move();
+
+
+                        timmy.move();
+                        timmy.turnLeft();
+                    }
+                    while (true) {
+                        if (timmy.getAvenue() > 0) {
+                            timmy.move();
+                       
+                    
+
+
+                    if (timmy.getAvenue() == 0) {
+                        timmy.turnLeft();
+                    timmy.turnLeft();
+                timmy.turnLeft();
+                timmy.move();
+                ti
+                    }               }
+            }
+            {
+                break;
+
+            }
+        }
+        while (true) {
+            if (timmy.getAvenue() == 0) {
+                timmy.turnLeft();
+                
+                timmy.move();
+            }
+        }
     }
 }
